650e12f0977bb3c774076623f5070395f68a4a53
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2022-10-15 09:58:11 -0300

    project init

---
+++ src/main/java/com/mheducation/SwServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mheducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mheducation/SwServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mheducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
